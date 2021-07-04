abb435a3afa0e815a4a2910923f909a70a00153c
ianklebold <ianstgo@gmail.com>
2021-07-04 16:40:49 -0300

    Este es el mensaje correcto

--- src/main/java/com/informatorio/ejemplo/repository/PlantaRepository.java
+++ src/main/java/com/informatorio/ejemplo/repository/PlantaRepository.java
@@ -1,14 +1,13 @@
 package com.informatorio.ejemplo.repository;
 
-import com.informatorio.ejemplo.entity.Persona;
+import com.informatorio.ejemplo.entity.Planta;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-//Con esto vamos a poder hacer los CRUD de una clase creada en entity a la BD puesta en 
-// resources/application.properties
+
 @Repository
-public interface EmpleadoRepository extends CrudRepository<Persona, Long>{
+public interface PlantaRepository extends CrudRepository<Planta, Long>{
     /*
         CrudRepository requiere de dos clases, el  primero que clase vamos a hacer CRUD
         y segundo que tipo de dato tiene la clave de la entidad
@@ -16,4 +15,3 @@ public interface EmpleadoRepository extends CrudRepository<Persona, Long>{
 
     
 }
-
